59d1d3fbf0a750de0b094a72be2a2d7d4248fb42
Jan Joneš <jjones@outlook.cz>
2020-03-22 11:46:08 +0100

    Reproduce bug

---
+++ RemoveIfEmptyTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace WhiteSpaceWriter_bug
+{
+    [HtmlTargetElement(Attributes = "remove-if-empty")]
+    public class RemoveIfEmptyTagHelper : TagHelper
+    {
+        public bool RemoveIfEmpty { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!RemoveIfEmpty)
+                return;
+
+            var content = await output.GetChildContentAsync(NullHtmlEncoder.Default);
+            if (content.IsEmptyOrWhiteSpace)
+                output.SuppressOutput();
+        }
+    }
+}
